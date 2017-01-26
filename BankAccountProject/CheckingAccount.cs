26694dedbdabc9559b52af25db2342e2929b4a7b
JCarg <Jcarg108@gmail.com>
2017-01-26 18:49:22 -0500

    Checking account method an properties added

--- BankAccountProject/CheckingAccount.cs
+++ BankAccountProject/CheckingAccount.cs
@@ -9,14 +9,14 @@ namespace BankAccountProject
     class CheckingAccount:Account
     {
         //Fields
-        private double checkingbalance = 3500.00;
+        private double checkingaccountbalance = 3500.00;
         private double totalcheckingdeposits;
 
        //Properties
        public double CheckingBalance
        {
-            get { return this.checkingbalance; }
-            set { this.checkingbalance = value; }
+            get { return this.checkingaccountbalance; }
+            set { this.checkingaccountbalance = value; }
        }
         public double TotalCheckingDeposit
         {
@@ -24,7 +24,18 @@ namespace BankAccountProject
             set { this.totalcheckingdeposits = value; }
         }
         
+        //Constructors
+        private CheckingAccount()
+        {
+            this.totalcheckingdeposits=
+        }
+        
         //Methods
+        public virtual static DepositingFunds()
+        {
+            double Deposit = int.Parse(Console.ReadLine());
+            this.checkingaccountbal
+        }
 
 
     }
