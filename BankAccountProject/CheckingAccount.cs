57bb67eb042e887501a2ea6fcd81382a9f05a956
JCarg <Jcarg108@gmail.com>
2017-01-26 12:36:25 -0500

    Added all classes

---
+++ BankAccountProject/CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class CheckingAccount:Account
+    {
+        //Fields
+        private double checkingbalance = 3500.00;
+        private double totalcheckingdeposits;
+
+       //Properties
+       public double CheckingBalance
+       {
+            get { return this.checkingbalance; }
+            set { this.checkingbalance = value; }
+       }
+        public double TotalCheckingDeposit
+        {
+            get { return this.totalcheckingdeposits; }
+            set { this.totalcheckingdeposits = value; }
+        }
+        
+        //Methods
+
+
+    }
+}
